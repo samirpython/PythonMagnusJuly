ef9cc5bc1cd72dfb8dd1d02d163a481f35b92fd6
samirpython <140649651+samirpython@users.noreply.github.com>
2023-07-31 00:30:19 -0400

    Day_4_26th_July_2023

---
+++ Day 4/ListExample.py
@@ -0,0 +1,21 @@
+a1 = [10,30,23,'abc',10.5,'hello']
+print(a1)
+print(a1[5])
+print(a1[0:3])
+print(a1[4:6])
+print(a1[3:6])
+print(a1[2:])
+print(a1[:5])
+print(a1)
+a1.append(120) #Appending or Adding the value at the end of the list.
+print(a1)
+a1.insert(3,'hi') #inserting the desired value at the specific Position.
+print(a1)
+a1[6]="welcome" #Replacing the value at specific position.
+print(a1)
+a1.remove('abc') #To remove the specific position from the index.
+print(a1)
+a1.pop() #to remove the last value from the list by default.
+print(a1)
+a1.pop(4) #To remove the specific index position value from the list.
+print(a1)
